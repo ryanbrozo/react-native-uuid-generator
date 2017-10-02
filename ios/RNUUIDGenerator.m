5ce0bb1b5811faf30ee4001136535fdbc152d316
Ryan Brozo <ryanbrozo@gmail.com>
2017-10-02 11:13:54 +0800

    Added getRandomBase64UUID in iOS

--- ios/RNUUIDGenerator.m
+++ ios/RNUUIDGenerator.m
@@ -15,5 +15,20 @@ RCT_EXPORT_METHOD(getRandomUUID:(RCTResponseSenderBlock)callback)
   callback(@[uuid]);
 }
 
+RCT_EXPORT_METHOD(getRandomBase64UUID:(BOOL)urlSafe callback:(RCTResponseSenderBlock)callback)
+{
+  NSUUID *identifier = [NSUUID UUID];
+  uuid_t uuid;
+  [identifier getUUIDBytes:uuid];
+  NSData *originalData = [NSData dataWithBytes:uuid length:16];
+  NSString *base64String = [originalData base64EncodedStringWithOptions:NSDataBase64Encoding64CharacterLineLength];
+  if (urlSafe) {
+    base64String = [base64String stringByReplacingOccurrencesOfString:@"/" withString:@"_"];
+    base64String = [base64String stringByReplacingOccurrencesOfString:@"+" withString:@"-"];
+  }
+    
+  callback(@[base64String]);
+}
+
 @end
 
