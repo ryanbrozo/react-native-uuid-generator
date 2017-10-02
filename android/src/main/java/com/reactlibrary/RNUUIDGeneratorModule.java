e8073d693d811a22e7b83cc1316962272298bd3f
Ryan Brozo <ryanbrozo@gmail.com>
2017-10-02 17:19:34 +0800

    Added getRandomBase64UUID in Android code

--- android/src/main/java/com/reactlibrary/RNUUIDGeneratorModule.java
+++ android/src/main/java/com/reactlibrary/RNUUIDGeneratorModule.java
@@ -1,11 +1,13 @@
 package com.reactlibrary;
 
+import android.util.Base64;
+
 import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.Callback;
 
+import java.nio.ByteBuffer;
 import java.util.UUID;
 
 public class RNUUIDGeneratorModule extends ReactContextBaseJavaModule {
@@ -27,5 +29,23 @@ public class RNUUIDGeneratorModule extends ReactContextBaseJavaModule {
     String uuid = UUID.randomUUID().toString();
 
     callback.invoke(uuid);
+  }
+  
+  @ReactMethod
+  public void getRandomBase64UUID(Boolean urlSafe, Callback callback) {
+    UUID uuid = UUID.randomUUID();
+    ByteBuffer bb = ByteBuffer.wrap(new byte[16]);
+    bb.putLong(uuid.getMostSignificantBits());
+    bb.putLong(uuid.getLeastSignificantBits());
+
+    int flag = 0;
+    if (urlSafe) {
+      flag = Base64.URL_SAFE | Base64.NO_WRAP | Base64.NO_PADDING;
+    }
+    else {
+      flag = Base64.NO_WRAP | Base64.NO_PADDING;
+    }
+    callback.invoke(Base64.encodeToString(bb.array(), flag));
+
   }
 }
